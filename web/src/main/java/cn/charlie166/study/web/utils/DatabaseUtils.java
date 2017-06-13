cb9761e93d43b034f7e7405ec7967b604f49d4ea
charlie166 <charlie166@163.com>
2017-06-13 21:30:18 +0800

    添加数据库连接

---
+++ web/src/main/java/cn/charlie166/study/web/utils/DatabaseUtils.java
@@ -0,0 +1,39 @@
+package cn.charlie166.study.web.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+* @ClassName: DatabaseUtils 
+* @Description: 数据库操作工具类
+* @company 
+* @author liyang
+* @Email charlie166@163.com
+* @date 2017年6月13日 
+*
+ */
+public class DatabaseUtils {
+
+	private final static String driver = PropertiesUtils.getVal("db.driver");
+	private final static String url = PropertiesUtils.getVal("db.url");
+	private final static String user = PropertiesUtils.getVal("db.username");
+	private final static String pwd = PropertiesUtils.getVal("db.password");
+	
+	static {
+		try {
+			Class.forName(driver);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public Connection getConnection(){
+		try {
+			DriverManager.getConnection(url, user, pwd);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
