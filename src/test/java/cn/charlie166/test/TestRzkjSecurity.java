74c7ff3a7c5155913a72bb7dd63d54ce4e2d6f23
charlie166 <charlie166@163.com>
2020-04-21 20:22:26 +0800

    微改

---
+++ src/test/java/cn/charlie166/test/TestRzkjSecurity.java
@@ -0,0 +1,28 @@
+package cn.charlie166.test;
+
+import com.rz.cms.sys.security.SecurityManage;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+@Slf4j
+public class TestRzkjSecurity {
+
+    @Test
+    public void test() {
+        log.debug("测试开始...");
+        String expireDateStart = "20190801";
+        String expireDateEnd = "20210201";
+        String mac = "E84E06025CFB";
+        try {
+            String s1 = SecurityManage.encryptUse3Des(mac);
+            String s2 = SecurityManage.encryptUse3Des(expireDateStart);
+            String s3 = SecurityManage.encryptUse3Des(expireDateEnd);
+            String s4 = s2 + s1 + s3;
+            String s5 = SecurityManage.encryptUseBaseMD5(s4);
+            log.debug("CODE:{}", s4);
+            log.debug("结果:{}", s5);
+        } catch (Exception e) {
+            log.error("操作失败", e);
+        }
+    }
+}
